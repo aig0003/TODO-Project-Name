588a7cf52bcb424c9aef7a0f404910e024a3e3c1
Unknown <thatcherr2@gmail.com>
2018-02-10 11:49:27 -0600

    Http request
    makes an http request to darksky

--- app-todo-team-name/src/app/app.module.ts
+++ app-todo-team-name/src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import {HttpClientModule, HttpClient} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
